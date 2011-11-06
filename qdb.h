d5f6bd5bd9f950b12ca516d605a1abb0fbb7d74f
Erfan <erfan@Erfankam-linux-0701.(none)>
2011-11-07 01:52:46 +0330

    add some api interface

---
+++ qdb.h
@@ -0,0 +1,9 @@
+# include <mysqldb.h>
+
+
+
+int Qdb_create_database();
+
+int Qdb_create_table();
+
+int Qdb_config_database();
--- qtime.h
+++ qtime.h
@@ -1,8 +1,19 @@
+/* This show resulation of time in nano second *
+/* in future we should get time from ntp server */
 
-int get_data_time();
+/* maybe */
+# include <time.h>
 
-int get_current_time();
+int resulation = -9
 
-int set_current_time();
+int Qtime_get_current_data_ime();
 
+int Qtime_set_current_data_time();
 
+int Qtime_get_current_time();
+
+int Qtime_set_current_time();
+
+int Qtime_set_current_date();
+
+int Qtime_get_current_date();
